dc93b5114ae05c44a30593294d56396572e83774
sumitk-mridha <sumitk.mridha@gmail.com>
2026-05-13 00:20:12 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    let msg = $state('Hello world');
+</script>
+
+<div style="background-color: lightgray; padding: 20px;">
+    typing...
+</div>
+<div>
+    <input type="text" bind:value={msg} placeholder="Type something..." />
+</div>
