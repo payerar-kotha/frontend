1a5202e02448249c92022375e6e241e207e92f0e
sumitk-mridha <sumitk.mridha@gmail.com>
2026-05-14 00:25:43 -0400

    websockets integrated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,34 @@
-<script>
-    let msg = $state('Hello world');
+<script lang="ts">
+    import { onDestroy } from 'svelte';
+    import { PUBLIC_BASEURL_LOCAL } from '$env/static/public';
+
+    let socket: WebSocket;
+    let messages: string[] = $state([]);
+    let msg = $state('');
+
+    const connectToTopic = () => {
+        if(socket) socket.close();
+        messages = [];
+        socket = new WebSocket('ws://localhost:8000/ws/technology');
+
+        socket.onmessage = (event) => {
+            messages = [...messages, event.data];
+        };
+    }
+
+    const publishMessage = () => {
+        msg = msg.trim();
+        if(socket && socket.readyState === WebSocket.OPEN) {
+            socket.send(msg);
+            msg = '';
+        }
+    }
+
+    connectToTopic();
+
+    onDestroy(() => {
+        if(socket) socket.close();
+    });
 </script>
 
 <div style="background-color: lightgray; padding: 20px;">
@@ -7,4 +36,14 @@
 </div>
 <div>
     <input type="text" bind:value={msg} placeholder="Type something..." />
+    <button onclick={publishMessage}>Submit</button>
+
+    <div>
+        <h4>Messages:</h4>
+        <ul>
+            {#each messages as message}
+                <li>{message}</li>
+            {/each}
+        </ul>
+    </div>
 </div>
