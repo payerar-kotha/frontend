8acff9e6ebdf1d107f618b6ab3bc35f2f0b4d9ba
sumitk-mridha <sumitk.mridha@gmail.com>
2026-05-22 00:39:17 -0400

    changes - user now has to create/join a channel before they can start talking

---
+++ src/components/Modal.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+
+    let { onClose, children }: { onClose: () => void, children: Snippet } = $props();
+
+    const close = () => {
+        onClose()
+    };
+</script>
+
+<div class="backdrop">
+    <div class="modal">
+        <button class="close-button" onclick={() => close()}>×</button>
+        {@render children()}
+    </div>
+</div>
+
+<style>
+    .backdrop {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .modal {
+        background-color: white;
+        padding: 20px;
+        border-radius: 8px;
+        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+        position: relative;
+    }
+
+    .close-button {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        padding: 10px;
+        box-sizing: border-box;
+        background: none;
+        border: none;
+        font-size: 20px;
+        cursor: pointer;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,57 @@
 <script lang="ts">
     import { onMount, onDestroy } from 'svelte';
     import { browser } from '$app/environment';
+
     import { PUBLIC_BASEURL_PROD } from '$env/static/public';
+    import Modal from "../components/Modal.svelte";
+
+    let isActiveInChannel = $state(false);
+    let createChannelModalShow = $state(false);
+    let joinChannelModalShow = $state(false);
+    let channelName = $state('');
+    let passcode = $state('');
+    let name = $state('');
+    let id = '';
 
     let socket: WebSocket;
     let messages: string[] = $state([]);
     let msg = $state('');
 
+    const createChannelModalClosedCB = () => {
+        createChannelModalShow = false;
+    }
+
+    const joinChannelModalClosedCB = () => {
+        joinChannelModalShow = false;
+    }
+
+    const createChannel = () => {
+        connectToTopic();
+        createChannelModalShow = false;
+    }
+
+    const joinChannel = () => {
+        connectToTopic();
+        joinChannelModalShow = false;
+    }
+
     const connectToTopic = () => {
+        isActiveInChannel = true;
         if(socket) socket.close();
         messages = [];
-        socket = new WebSocket(`wss://${PUBLIC_BASEURL_PROD}/ws/technology`);
+        socket = new WebSocket(`wss://${PUBLIC_BASEURL_PROD}/join-channel/${channelName}?passcode=${passcode}&name=${name}&id=${id}`);
 
         socket.onmessage = (event) => {
             messages = [...messages, event.data];
         };
 
-        socket.onclose = () => {
-            console.log('WebSocket connection closed');
+        socket.onclose = (e) => {
+            console.log('WebSocket connection closed', e);
+            if(e.code == 1008) {
+                console.error(e.reason);
+                localStorage.clear();
+                isActiveInChannel = false;
+            }
         };
 
         socket.onerror = (error) => {
@@ -36,12 +70,22 @@
 
     const reconnect = () => {
         if (!socket || socket.readyState === WebSocket.CLOSED) {
+            if(!channelName) channelName = localStorage.getItem('topic') || '';
+            if(!passcode) passcode = localStorage.getItem('passcode') || '';
+            if(!name) name = localStorage.getItem('name') || '';
+            if(!id) id = localStorage.getItem('id') || '';
             connectToTopic();
         }
     };
 
     onMount(() => {
         if(!browser) return;
+        if(!isActiveInChannel) return;
+        if(!localStorage.getItem('topic') || !localStorage.getItem('passcode')){
+            localStorage.clear();
+            isActiveInChannel = false;
+            return;
+        }
         connectToTopic();
         messages = [];
         window.addEventListener('online', reconnect);
@@ -56,19 +100,47 @@
     });
 </script>
 
-<div style="background-color: lightgray; padding: 20px;">
-    typing...
-</div>
-<div>
-    <input type="text" bind:value={msg} placeholder="Type something..." />
-    <button onclick={publishMessage}>Submit</button>
-
+{#if !isActiveInChannel}
     <div>
-        <h4>Messages:</h4>
-        <ul>
-            {#each messages as message}
-                <li>{message}</li>
-            {/each}
-        </ul>
+        <button disabled>Log in and go back to your saved chats.</button>
+        <button onclick={() => createChannelModalShow = true}>Create New Anonymous Channel</button>
+        <button onclick={() => joinChannelModalShow = true}>Join Existing Anonymous Channel</button>
     </div>
-</div>
+
+    {#if createChannelModalShow}
+        <Modal onClose={() => createChannelModalClosedCB()} >
+            <h2>Create New Anonymous Channel</h2>
+            <input type="text" placeholder="Channel Name" bind:value={channelName} />
+            <input type="text" placeholder="Passcode (optional)" bind:value={passcode} />
+            <input type="text" placeholder="Your Name (optional)" bind:value={name} />
+            <button onclick={() => createChannel()}>Create Channel</button>
+        </Modal>
+    {/if}
+
+    {#if joinChannelModalShow}
+        <Modal onClose={() => joinChannelModalClosedCB()} >
+            <h2>Join Existing Anonymous Channel</h2>
+            <input type="text" placeholder="Channel Name" bind:value={channelName} />
+            <input type="text" placeholder="Passcode" bind:value={passcode} />
+            <input type="text" placeholder="Your Name (optional)" bind:value={name} />
+            <button onclick={() => joinChannel()}>Join Channel</button>
+        </Modal>
+    {/if}
+{:else}
+    <div style="background-color: lightgray; padding: 20px;">
+        typing...
+    </div>
+    <div>
+        <input type="text" bind:value={msg} placeholder="Type something..." />
+        <button onclick={publishMessage}>Submit</button>
+
+        <div>
+            <h4>Messages:</h4>
+            <ul>
+                {#each messages as message}
+                    <li>{message}</li>
+                {/each}
+            </ul>
+        </div>
+    </div>
+{/if}
